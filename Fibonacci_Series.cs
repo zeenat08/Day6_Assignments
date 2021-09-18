0a15709076becce28571dacea4d995e9e766a3a8
zeenat08 <89180148+zeenat08@users.noreply.github.com>
2021-09-18 22:35:55 +0530

    Zeenat: added fibonacci series

---
+++ Fibonacci_Series.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day6_LogicalProblems
+{
+    class Fibonacci_Series
+    {
+        public static void CalculateFibonacci()
+        {
+            int num1 = 0, num2 = 1;
+            Console.Write("Enter the number of elements to print: ");
+            int numberOfElements = int.Parse(Console.ReadLine());
+            Console.Write(num1 + " " + num2 + " ");
+            for(int i=2; i<=numberOfElements; i++)
+            {
+                int nextNumber = num1 + num2;
+                Console.Write(nextNumber+ " ");
+                num1 = num2;
+                num2 = nextNumber;
+            }
+        }
+    }
+}
