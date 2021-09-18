5798f28d828320ba78f68140ef5404867b644f9b
zeenat08 <89180148+zeenat08@users.noreply.github.com>
2021-09-18 23:15:31 +0530

    Zeenat: added prime number

---
+++ Prime_Number.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day6_LogicalProblems
+{
+    class Prime_Number
+    {
+        public static void IsPrime()
+        {
+            int num;
+            Console.Write("Enter a number to check Prime or not: ");
+            num = int.Parse(Console.ReadLine());
+            for(int i=2; i<=num/2; i++)
+            {
+                if(num%i == 0)
+                {
+                    Console.WriteLine( num + " is not a Prime Number");
+                    return;
+                } 
+            }
+            Console.WriteLine(num + " is a Prime Number");
+        }
+    }
+}
