b6170055cc665ab0e8762fb408efb31561e30632
zeenat08 <89180148+zeenat08@users.noreply.github.com>
2021-09-18 23:14:56 +0530

    Zeenat: added reverse number

---
+++ Reverse_Number.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day6_LogicalProblems
+{
+    class Reverse_Number
+    {
+        public static void CheckReverse()
+        {
+            Console.Write("Enter a number u want to reverse: ");
+            int num = Convert.ToInt32(Console.ReadLine());
+            int revNum = 0;
+            while(num != 0)
+            {
+                int digit = num % 10;
+                revNum = revNum * 10 + digit;
+                num = num / 10;
+            }
+            Console.WriteLine($"Reverse of the number is: {revNum}");
+        }
+    }
+}
