7834090308f4363ee92e8eb30226823f6010f0ea
zeenat08 <89180148+zeenat08@users.noreply.github.com>
2021-09-18 23:21:43 +0530

    Zeenat: added stopwatch programm

---
+++ stopwatch.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading;
+using System.Diagnostics;
+
+namespace LogicalPrograms
+{
+	class Stopwatch
+	{
+		public static void TimeElapsed()
+		{
+			System.Diagnostics.Stopwatch timer = new System.Diagnostics.Stopwatch();
+			timer.Start();
+			for (int i = 0; i < 100; i++)
+			{
+				Thread.Sleep(2);
+			}
+			timer.Stop();
+			Console.WriteLine(" Time elapsed: {0} ", timer.Elapsed);
+		}
+	}
+
+}
