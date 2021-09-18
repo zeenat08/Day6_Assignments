8bcebc89890438cc37b0aeb44fcaa2fde984a332
zeenat08 <89180148+zeenat08@users.noreply.github.com>
2021-09-18 23:14:19 +0530

    Zeenat: added perfect number

---
+++ Perfect_Number.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day6_LogicalProblems
+{
+    class Perfect_Number
+    {
+        public static void IsPerfect()
+        {
+            int sum = 0;
+            Console.Write("Enter a number to check Perfect number or not: ");
+            int num = int.Parse(Console.ReadLine());
+            for(int i=1; i<num; i++)
+            {
+                if(num%i==0)
+                {
+                    sum = sum + i;
+                }
+            }
+            if(num==sum)
+            {
+                Console.WriteLine($"{num} is a perfect number");
+            }
+            else
+            {
+                Console.WriteLine($"{num} is not a perfect number");
+            }
+        }
+    }
+}
