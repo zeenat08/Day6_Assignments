579be6d41e29aa0395797d1733b3b67999440e43
zeenat08 <89180148+zeenat08@users.noreply.github.com>
2021-09-18 23:30:24 +0530

    Zeenat: added main method

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+namespace Day6_LogicalProblems
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Fibonacci_Series.CalculateFibonacci();
+            //Prime_Number.IsPrime();
+            //Reverse_Number.CheckReverse();
+            //Perfect_Number.IsPerfect();
+            //Coupon_Number.CouponNumbers();
+            Stopwatch.StopWatch();
+
+        }    
+    }
+}
